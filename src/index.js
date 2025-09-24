d80b647c1e75d6733948535f46f58f8107165be7
nitin00201 <kabinitin2002@gmail.com>
2025-09-24 19:08:38 +0530

    added prod url

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-const allowedOrigins = ["http://localhost:3000", "https://myapp.com"];
+const allowedOrigins = ["http://localhost:3000", "https://svara-ai-frontend-ruby.vercel.app/"];
 
 app.use(
   cors({
